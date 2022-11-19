08380fe37e8d3637081e9fc22ed2cf5dbbc59732
fourhtyoz <fourhtyoz@gmail.com>
2022-11-19 15:15:10 +0300

    1 task and 2 task

--- 1 task/index.js
+++ 1 task/index.js
@@ -22,13 +22,11 @@ function convertSeconds(seconds) {
   // Конвертация секунд в удобный формат даты: ч:м:с
   let h = Math.floor(seconds / 3600);
   let m = Math.floor(seconds % 3600 / 60);
-  let s = Math.floor(seconds % 3600 % 60);
-  
+  let s = Math.floor(seconds % 3600 % 60);  
   // Более красивый формат в виде чч:мм:сс
   if (h < 10) h = `0${h}`
   if (m < 10) m = `0${m}`
   if (s < 10) s = `0${s}`
-  
   timerEl.innerHTML = `${h}:${m}:${s}`;
 }
 
